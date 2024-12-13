e63d5c69e515e79c49274cc068ce6a14e7656351
mrkaan <mrkaan@qq.com>
2024-12-13 12:38:10 +0800

    feat: 压缩工具更新

--- code/utils.py
+++ code/utils.py
@@ -12,15 +12,15 @@ import zipfile
 import shutil
 from pathlib import Path
 
+"""检查目录下是否存在指定扩展名的文件"""
 def check_files(directory, exts):
-    """检查目录下是否存在指定扩展名的文件"""
     files = []
     for ext in exts:
         files.extend(glob(os.path.join(directory, f'*.{ext}')))
     return files
 
+"""验证Excel文件格式是否正确"""
 def validate_excel(df, strictness=1):
-    """验证Excel文件格式是否正确"""
     required_columns = ['shortCut', 'meaning', 'originalFile']
     
     if not all(col in df.columns for col in required_columns):
@@ -38,8 +38,14 @@ def validate_excel(df, strictness=1):
             print("表格式错误：存在重复项")
             exit(1)
 
+"""根据分组目录下的excel文件生成 EmotionConfig.json文件并判断图片文件是否存在"""
 def manage_config_json(directory, mode='new', strictness=1):
-    """管理EmotionConfig.json文件"""
+    """
+        :param directory: 分组目录
+        :param mode: 处理模式，'new'为新建，'append'为追加
+        :param strictness: 严格模式 1是否有控制  2是否有重复项
+        :return: True表示成功 None表示失败
+    """
 
     # 判断directory变量是否为空
     if not directory:
@@ -97,27 +103,87 @@ def manage_config_json(directory, mode='new', strictness=1):
     print(f"成功生成 {config_path}")
     return True
 
-# def json_to_xlsx(output_path='output.xlsx'):
-#     """将config.json保存为xlsx文件"""
 
-#     # json_path默认为当前目录下的文件
-#     json_path = os.path.join(os.getcwd(), 'EmotionConfig.json')  # 使用完整路径
+# 检查EmotionConfig.json文件是否合法 是否存在、是否有重复项（shortCut、originalFile）、是否有空值（meaning、hortCut、originalFile）、图片文件是否存在
+def validate_config_json(directory):
+    """
+        :param directory: 分组目录
+        :return: True表示成功 None表示失败
+    """
 
-#     # 判断json_path是否存在
-#     if not os.path.exists(json_path):
-#         print(f"文件 {json_path} 不存在")
-#         return None
+    # 判断directory变量是否为空
+    if not directory:
+        print("目录不能为空")
+        return None
+
+    # 判断directory是否存在
+    if not os.path.exists(directory):
+        print(f"目录 {directory} 不存在")
+        return None
+
+    # 判断EmotionConfig.json文件是否存在
+    config_path = os.path.join(directory, 'EmotionConfig.json')
+    if not os.path.exists(config_path):
+        print(f"文件 {config_path} 不存在")
+        return None
     
-#     # 读取JSON文件内容
-#     with open(json_path, 'r') as file:
-#         data = json.load(file)
+    # 读取JSON文件内容
+    with open(config_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
     
-#     df = pd.DataFrame(data)
-#     df.to_excel(output_path, index=False)
+    # 开始检查文件 有错并不停止而是检查完整个文件并提示错误位置
+    seen_files = set()
+    qualified = True
+
+    for index, entry in enumerate(data):
+        # 检查shortCut是否包含中文字符
+        if any('\u4e00' <= char <= '\u9fff' for char in entry['shortCut']):
+            qualified = False
+            print(f"表格错误：位置 {index} 内容 {entry['shortCut']} 表情的短语不能包含中文字符")
+        
+        # 检查meaning是否为空
+        if not entry['meaning']:
+            qualified = False
+            print(f"表格错误：位置 {index} 内容 {entry['meaning']} 表情的释义不能为空")
+        
+        # 检查originalFile是否存在
+        original_file = os.path.join(directory, entry['originalFile'])
+        if not os.path.exists(original_file):
+            qualified = False
+            print(f"表格错误：位置 {index} 内容 {entry['shortCut']} 表情的图片文件 {original_file} 不存在")
+        
+        # 检查originalFile是否重复
+        if entry['originalFile'] in seen_files:
+            qualified = False
+            print(f"表格错误：位置 {index} 内容 {entry['shortCut']} 表情的图片文件 {original_file} 重复")
+        
+        seen_files.add(entry['originalFile'])
+    
+    return qualified
 
 
+
+"""将EmotionConfig.json保存为xlsx文件"""
+def json_to_xlsx(output_path='output.xlsx'):
+
+    # json_path默认为当前目录下的文件
+    json_path = os.path.join(os.getcwd(), 'EmotionConfig.json')  # 使用完整路径
+
+    # 判断json_path是否存在
+    if not os.path.exists(json_path):
+        print(f"文件 {json_path} 不存在")
+        return None
+    
+    # 读取JSON文件内容
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+    
+    df = pd.DataFrame(data)
+    df.to_excel(output_path, index=False)
+
+
+"""将指定目录下的内容压缩为.zip并重命名为.emo"""
 def compress_to_emo(directory, file_name, output_filename="CustomEmo"):
-    """将指定目录下的内容压缩为.zip并重命名为.emo"""
 
     # 判断directory变量是否为空
     if not directory:
@@ -168,8 +234,8 @@ def compress_to_emo(directory, file_name, output_filename="CustomEmo"):
     print(f"成功创建 {emo_filename}")
 
 
-def json_to_xlsx(json_path, output_path=None):
-    """将指定的JSON文件内容转换为XLSX文件"""
+"""将auto_customer项目下的hoststrings_cn.json文件转换为emo需要的EmotionConfig.json文件"""
+def json_to_xlsx_auto_customer(json_path, output_path=None):
     
     # 如果没有提供输出路径，则默认使用JSON文件所在目录
     if not output_path:
@@ -212,8 +278,8 @@ def json_to_xlsx(json_path, output_path=None):
     print(f"成功创建 {output_file}")
 
 
+"""将JSON中的文件复制到指定目录"""
 def copy_json_files(json_path, target_dir):
-    """将JSON中的文件复制到指定目录"""
     
     # 确保目标目录存在
     os.makedirs(target_dir, exist_ok=True)
@@ -260,29 +326,39 @@ def copy_json_files(json_path, target_dir):
 
 if __name__ == '__main__':
 
-    # 根据表格生成EmotionConfig.json文件
-    dir_path = r'C:\Users\A\Desktop\CustomEmo'
-    group_name = 'CustomEmo'
-
     print("开始执行")
-    success_flag = manage_config_json(f'{dir_path}\\{group_name}', mode='new', strictness=2)
+
+    # 以下为测试代码，请根据实际情况修改参数
+    from_excel = False  # 是否从Excel文件生成EmotionConfig.json文件
+    from_json = True  # 是否从JSON文件生成EmotionConfig.json文件
+    compress_emo = True  # 是否压缩EmotionConfig.json文件为.emo文件
+    copy_files = False  # 是否将EmotionConfig.json文件中的文件复制到指定目录
+    dir_path = r'C:\Users\A\Desktop\CustomEmo'  # 分组目录
+    group_name = 'CustomEmo'  # 分组名称
+
+    success_flag = None # 检查是否成功标志变量 不要修改
+
+    if from_excel:
+        success_flag = manage_config_json(f'{dir_path}\\{group_name}', mode='new', strictness=2)
+    if from_json:
+        success_flag = validate_config_json(f'{dir_path}\\{group_name}')
     # success_flag = False
-    if success_flag:
-        # 从后往前查找最后一个反斜杠的索引
-        last_backslash_index = dir_path.rfind('\\')
+    if success_flag and compress_emo:
+        # 从dir_path获取路径名和文件名
+        directory_name = os.path.dirname(dir_path)
+        file_name = os.path.basename(dir_path)
 
-        # 如果找到了反斜杠
-        if last_backslash_index != -1:
-            # 使用切片分别获取路径的两部分
-            part1 = dir_path[:last_backslash_index]  # 反斜杠之前的部分
-            part2 = dir_path[last_backslash_index + 1:]  # 反斜杠之后的部分
-            compress_to_emo(part1, part2, output_filename="CustomEmo")
-    
+        compress_to_emo(directory_name, file_name, output_filename="CustomEmo")
 
-    
-    # 将handles.json转换为output.xlsx文件
-    # json_to_xlsx(r'C:\note\python-WinGUI\config\hotstrings_cn.json')
 
+        # last_backslash_index = dir_path.rfind('\\')
+        # # 如果找到了反斜杠
+        # if last_backslash_index != -1:
+        #     # 使用切片分别获取路径的两部分
+        #     part1 = dir_path[:last_backslash_index]  # 反斜杠之前的部分
+        #     part2 = dir_path[last_backslash_index + 1:]  # 反斜杠之后的部分
+        #     compress_to_emo(part1, part2, output_filename="CustomEmo")
     
     # 将handles.json中的文件复制到指定目录
-    # copy_json_files(r'C:\note\python-WinGUI\config\hotstrings_cn.json', r'C:\Users\A\Desktop\CustomEmo\CustomEmo')
+    if copy_files:
+        copy_json_files(r'C:\note\python-WinGUI\config\hotstrings_cn.json', r'C:\Users\A\Desktop\CustomEmo\CustomEmo')
