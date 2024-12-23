817eab27e956177dd67da6ed32456f2a58331e49
mrkaan <mrkaan@qq.com>
2024-12-23 12:58:31 +0800

    feat: 启动项目快捷bat工具更新 窗口不会关闭

--- code/start_project.bat
+++ code/start_project.bat
@@ -19,16 +19,7 @@ echo AutoHotkey started successfully.
 cd /d %PYTHON_PROJECT_DIR%
 echo Directory changed successfully.
 
-:: 激活Conda环境并运行Python文件
-call conda activate gui && python %PYTHON_FILE_NAME%
-
-:: 下面这种方法会导致关闭cmd后python程序仍然运行 需要手动到任务管理器结束python进程 慎用
-:: 激活Conda环境并运行Python文件，忽略退出状态
-::call conda activate gui
-::start "" pythonw %PYTHON_FILE_NAME%
-::echo Python script started.
-:::: 暂停，等待用户按键
-::echo Press any key to exit the batch script...
-::pause >nul
+:: 创建一个新的CMD窗口来激活Conda环境并运行Python文件
+start cmd /k "call conda activate gui && python %PYTHON_FILE_NAME%"
 
 endlocal
